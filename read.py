ed9e0233d1d7faac61852ce31d1fbe489f82cde8
Qi-Mo Lin <qimolin9@gmail.com>
2022-08-13 14:26:42 +0200

    read images and videos

---
+++ read.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+
+# Reading image
+img = cv.imread('photos/cat_large2.jpg')
+
+cv.imshow('Cat', img)
+
+cv.waitKey(0)
+
+# Reading video
+capture = cv.VideoCapture('videos/dog.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    if isTrue:
+        cv.imshow('Video', frame)
+        if cv.waitKey(20) & 0xFF == ord('d'):
+            break
+    else:
+        break
+
+capture.release()
+cv.destroyAllWindows()
